4f2bd3ded4cdf698c41bd413897f16e48c53cb1f
Simeon Marchand <simeonrmarchand@gmail.com>
2023-05-10 14:50:44 -0500

    Calculated the BMI of the patient and displayed their BMI as well as if they are underweight, normal weight, overweight, or Obese.

--- src/BMICalculator.java
+++ src/BMICalculator.java
@@ -4,8 +4,8 @@ public class BMICalculator {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
 
-        //Take user input
-        System.out.print("Patient Name: ");
+        // Take user input
+        System.out.print("Patient First and Last Name: ");
         String name = scanner.nextLine();
 
         System.out.print("Patient birthdate (format: mm-dd-yyyy): ");
@@ -17,6 +17,22 @@ public class BMICalculator {
         System.out.print("Patient Height in meters: ");
         double height = scanner.nextDouble();
 
+        // Calculate the BMI and store this information
+        double bmi = weight / (height * height);
 
+        // Display the persons name and the BMI result
+        String category;
+        if (bmi < 18.5){
+            category = "Underweight";
+        } else if (bmi < 25){
+            category = "Normal Weight";
+        } else if (bmi < 30){
+            category = "Overweight";
+        } else {
+            category = "Obese";
+        }
+
+        System.out.println(name + "'s BMI Category is: " + category);
+        System.out.println(name + "'s BMI is: " + bmi);
     }
 }
